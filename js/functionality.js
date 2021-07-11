cd9e9c9fbe30bb806691de9dad74e7d7993ef5d7
Zac Remboldt <zac.remboldt@joinroot.com>
2021-07-10 19:39:30 -0500

    Small updates

--- js/functionality.js
+++ js/functionality.js
@@ -1,7 +1,7 @@
 const aboutMessage = `TODO: Make this a real modal... maybe ¯\\_(ツ)_/¯
 ---
 
-I created this page during Root's Q2 Hackdays, 2021 as a sandbox for mixing HTML and WebGL shaders.
+I created this page during Root's Q2 Hackdays, 2021 as a sandbox for mixing HTML and WebGL shaders. It's not perfect, there would definitely be more to consider if implementing this on a real site but this works for an experiment.
 
 Credits:
 - Images were found on unsplash.com
